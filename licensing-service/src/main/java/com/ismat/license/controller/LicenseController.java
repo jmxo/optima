b2c63aca18ddfc385ff8dada0d92262d6ba6498c
mo.ismat <mo.ismat@auto1.com>
2022-07-19 16:20:51 +0200

    add basic structure for licensing-service

---
+++ licensing-service/src/main/java/com/ismat/license/controller/LicenseController.java
@@ -0,0 +1,36 @@
+package com.ismat.license.controller;
+
+import com.ismat.license.model.License;
+import com.ismat.license.service.LicenseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "v1/organization/{organizationId}/license")
+public class LicenseController {
+
+    @Autowired
+    private LicenseService licenseService;
+
+    @GetMapping(value = "/{licenseId}")
+    public ResponseEntity<License> getLicense(@PathVariable("organizationId") String organizationId, @PathVariable("licenseId") String licenseId) {
+        License license = licenseService.getLicense(licenseId, organizationId);
+        return ResponseEntity.ok(license);
+    }
+
+    @PutMapping
+    public ResponseEntity<String> updateLicense(@PathVariable("organizationId") String organizationId, @RequestBody License request) {
+        return ResponseEntity.ok(licenseService.updateLicense(request, organizationId));
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createLicense(@PathVariable("organizationId") String organizationId, @RequestBody License request) {
+        return ResponseEntity.ok(licenseService.createLicense(request, organizationId));
+    }
+
+    @DeleteMapping(value = "/{licenseId}")
+    public ResponseEntity<String> deleteLicense(@PathVariable("organizationId") String organizationId, @PathVariable("licenseId") String licenseId) {
+        return ResponseEntity.ok(licenseService.deleteLicense(licenseId, organizationId));
+    }
+}
