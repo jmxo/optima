2de234e70b1b1046e142c08d112c32f2e35a79b6
mo.ismat <mo.ismat@auto1.com>
2022-07-19 17:03:11 +0200

    add hateoas links in get response

--- licensing-service/src/main/java/com/ismat/license/controller/LicenseController.java
+++ licensing-service/src/main/java/com/ismat/license/controller/LicenseController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Locale;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
 @RequestMapping(value = "v1/organization/{organizationId}/license")
 public class LicenseController {
@@ -18,6 +21,19 @@ public class LicenseController {
     @GetMapping(value = "/{licenseId}")
     public ResponseEntity<License> getLicense(@PathVariable("organizationId") String organizationId, @PathVariable("licenseId") String licenseId) {
         License license = licenseService.getLicense(licenseId, organizationId);
+        license.add(
+                linkTo(methodOn(LicenseController.class)
+                        .getLicense(organizationId, license.getLicenseId()))
+                        .withSelfRel(),
+                linkTo(methodOn(LicenseController.class)
+                        .createLicense(organizationId, license, null))
+                        .withRel("createLicense"),
+                linkTo(methodOn(LicenseController.class)
+                        .updateLicense(organizationId, license, null))
+                        .withRel("updateLicense"),
+                linkTo(methodOn(LicenseController.class)
+                        .deleteLicense(organizationId, license.getLicenseId(), null))
+                        .withRel("deleteLicense"));
         return ResponseEntity.ok(license);
     }
 
--- licensing-service/src/main/java/com/ismat/license/model/License.java
+++ licensing-service/src/main/java/com/ismat/license/model/License.java
@@ -4,9 +4,12 @@ package com.ismat.license.model;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.hateoas.RepresentationModel;
 
-@Getter @Setter @ToString
-public class License {
+@Getter
+@Setter
+@ToString
+public class License extends RepresentationModel<License> {
 
     private int id;
     private String licenseId;
