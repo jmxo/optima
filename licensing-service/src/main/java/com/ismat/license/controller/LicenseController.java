8e593ecaa86760096aae7763f7965bb49eb6bde7
mo.ismat <mo.ismat@auto1.com>
2022-07-19 16:21:34 +0200

    add localization

--- licensing-service/src/main/java/com/ismat/license/controller/LicenseController.java
+++ licensing-service/src/main/java/com/ismat/license/controller/LicenseController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Locale;
+
 @RestController
 @RequestMapping(value = "v1/organization/{organizationId}/license")
 public class LicenseController {
@@ -20,17 +22,17 @@ public class LicenseController {
     }
 
     @PutMapping
-    public ResponseEntity<String> updateLicense(@PathVariable("organizationId") String organizationId, @RequestBody License request) {
-        return ResponseEntity.ok(licenseService.updateLicense(request, organizationId));
+    public ResponseEntity<String> updateLicense(@PathVariable("organizationId") String organizationId, @RequestBody License request, @RequestHeader(value = "Accept-Language", required = false) Locale locale) {
+        return ResponseEntity.ok(licenseService.updateLicense(request, organizationId, locale));
     }
 
     @PostMapping
-    public ResponseEntity<String> createLicense(@PathVariable("organizationId") String organizationId, @RequestBody License request) {
-        return ResponseEntity.ok(licenseService.createLicense(request, organizationId));
+    public ResponseEntity<String> createLicense(@PathVariable("organizationId") String organizationId, @RequestBody License request, @RequestHeader(value = "Accept-Language", required = false) Locale locale) {
+        return ResponseEntity.ok(licenseService.createLicense(request, organizationId, locale));
     }
 
     @DeleteMapping(value = "/{licenseId}")
-    public ResponseEntity<String> deleteLicense(@PathVariable("organizationId") String organizationId, @PathVariable("licenseId") String licenseId) {
-        return ResponseEntity.ok(licenseService.deleteLicense(licenseId, organizationId));
+    public ResponseEntity<String> deleteLicense(@PathVariable("organizationId") String organizationId, @PathVariable("licenseId") String licenseId, @RequestHeader(value = "Accept-Language", required = false) Locale locale) {
+        return ResponseEntity.ok(licenseService.deleteLicense(licenseId, organizationId, locale));
     }
 }
--- licensing-service/src/main/java/com/ismat/license/service/LicenseService.java
+++ licensing-service/src/main/java/com/ismat/license/service/LicenseService.java
@@ -1,12 +1,18 @@
 package com.ismat.license.service;
 
 import com.ismat.license.model.License;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 
+import java.util.Locale;
 import java.util.Random;
 
 @Service
 public class LicenseService {
+
+    @Autowired
+    MessageSource messages;
 
     public License getLicense(String licenseId, String organizationId) {
         License license = new License();
@@ -19,27 +25,27 @@ public class LicenseService {
         return license;
     }
 
-    public String createLicense(License license, String organizationId) {
+    public String createLicense(License license, String organizationId, Locale locale) {
         String responseMessage = null;
         if (license != null) {
             license.setOrganizationId(organizationId);
-            responseMessage = String.format("This is the post and the object is: %s", license.toString());
+            responseMessage = String.format(messages.getMessage("license.create.message", null, locale), license.toString());
         }
         return responseMessage;
     }
 
-    public String updateLicense(License license, String organizationId) {
+    public String updateLicense(License license, String organizationId, Locale locale) {
         String responseMessage = null;
         if (license != null) {
             license.setOrganizationId(organizationId);
-            responseMessage = String.format("This is the put and the object is: %s", license.toString());
+            responseMessage = String.format(messages.getMessage("license.update.message", null, locale), license.toString());
         }
         return responseMessage;
     }
 
-    public String deleteLicense(String licenseId, String organizationId) {
+    public String deleteLicense(String licenseId, String organizationId, Locale locale) {
         String responseMessage = null;
-        responseMessage = String.format("Deleting license with id %s for the organization %s", licenseId, organizationId);
+        responseMessage = String.format(messages.getMessage("license.delete.message", null, locale), licenseId, organizationId);
         return responseMessage;
     }
 
