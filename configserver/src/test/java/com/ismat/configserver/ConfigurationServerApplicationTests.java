9ae62330ee5ef0f1ee300bc56405fc1d5af99330
mo.ismat <mo.ismat@auto1.com>
2022-07-19 19:35:49 +0200

    add config server project

---
+++ configserver/src/test/java/com/ismat/configserver/ConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ismat.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
