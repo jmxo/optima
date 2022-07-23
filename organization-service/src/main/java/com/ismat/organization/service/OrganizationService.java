785697064619e5c49f8d814123de44ea540594d5
mo.ismat <mo.ismat@auto1.com>
2022-07-23 21:19:09 +0200

    add organization-service and register with eureka

---
+++ organization-service/src/main/java/com/ismat/organization/service/OrganizationService.java
@@ -0,0 +1,37 @@
+package com.ismat.organization.service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ismat.organization.model.Organization;
+import com.ismat.organization.repository.OrganizationRepository;
+
+@Service
+public class OrganizationService {
+
+    @Autowired
+    private OrganizationRepository repository;
+
+    public Organization findById(String organizationId) {
+        Optional<Organization> opt = repository.findById(organizationId);
+        return (opt.isPresent()) ? opt.get() : null;
+    }
+
+    public Organization create(Organization organization){
+        organization.setId( UUID.randomUUID().toString());
+        organization = repository.save(organization);
+        return organization;
+
+    }
+
+    public void update(Organization organization){
+        repository.save(organization);
+    }
+
+    public void delete(Organization organization){
+        repository.deleteById(organization.getId());
+    }
+}
