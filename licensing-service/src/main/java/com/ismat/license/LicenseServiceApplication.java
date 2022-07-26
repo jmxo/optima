f97675a6c98142d5ad93b45361a5875cb15abdee
mo.ismat <mo.ismat@auto1.com>
2022-07-26 03:39:14 +0200

    listen to message on license service

--- licensing-service/src/main/java/com/ismat/license/LicenseServiceApplication.java
+++ licensing-service/src/main/java/com/ismat/license/LicenseServiceApplication.java
@@ -1,23 +1,40 @@
 package com.ismat.license;
 
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
-import java.util.Locale;
+import com.ismat.license.events.model.OrganizationChangeModel;
 
 @SpringBootApplication
 @EnableFeignClients
 @RefreshScope
+@EnableBinding(Sink.class)
 public class LicenseServiceApplication {
+
+	private static final Logger logger = LoggerFactory.getLogger(LicenseServiceApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(LicenseServiceApplication.class, args);
+	}
+
+	@StreamListener(Sink.INPUT)
+	public void loggerSink(OrganizationChangeModel orgChange) {
+		logger.debug("Received {} event for the organization id {}",
+				orgChange.getAction(),
+				orgChange.getOrganizationId());
 	}
 
 	@Bean
